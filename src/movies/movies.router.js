e870b7b8894526fdedec5af83ba28a4b578db89b
SheykoWk <sheyko.wk@gmail.com>
2022-10-03 22:10:57 -0500

    initial commit

---
+++ src/movies/movies.router.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+//? Este es el prefijo: /movies
+
+
+
+router.get('/') //? /movies/
+router.post('/') //? /movies/
+
+router.get('/:id') //? /movies/:id
+router.delete('/:id') //? /movies/:id
+router.patch('/:id') //? /movies/:id
+router.put('/:id') //? /movies/:id
+
+module.exports = router
