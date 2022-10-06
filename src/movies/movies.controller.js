70f5c41cf6ef4ee8ddd4b2d517f6658e4ce05127
SheykoWk <sheyko.wk@gmail.com>
2022-10-05 21:42:23 -0500

    add controllers

---
+++ src/movies/movies.controller.js
@@ -0,0 +1,36 @@
+
+const  uuid = require('uuid')
+const Movies = require('../models/movies.models')
+
+const getAllMovies =  () => {
+    const data =  Movies.findAll() //? Traer todas las peliculas
+    //? Select * from movies;
+    return data
+}
+
+// getAllMovies()
+//     .then((response) => console.log(response))
+//     .catch((err) => console.log(err))
+
+const createMovie = async (data) => {
+    const newMovie = await Movies.create({
+        id: uuid.v4(),
+        name: data.name,
+        genre: data.genre,
+        duration: data.duration,
+        releaseDate: data.releaseDate
+    })
+    //? insert into movies (id, name, genre, duration, releaseDate) values (uuid.v4(), data.name, data.genre, data.duration, data.releaseDate)
+    return newMovie
+}
+
+createMovie({
+    name: 'Pacific Rim',
+    genre: 'Action, SciFi',
+    duration: 120,
+    releaseDate: '2012/10/30'
+})
+    .then(response => console.log(response))
+    .catch(err => console.log(err))
+
+
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize')
+const config = require('../config')
 
 const db = new Sequelize({
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'root',
-    database: 'movies_crud'
+    host: config.db.host, //? Variable de entorno del host
+    username: config.db.username, //? Variable de entorno del usuario
+    password: config.db.password, //? Variable de entorno de la contraseña
+    database: config.db.name //? Variable de entorno de la base de datos
 })
 
 module.exports = db
